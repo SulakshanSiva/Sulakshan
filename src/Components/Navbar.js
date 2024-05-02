b3d7378e8afb5cda778683f7aa25f52055abd477
Sulakshan Sivakumaran <sulakshansivak@gmail.com>
2024-05-02 13:20:42 -0400

    made navbar responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,63 @@
-  import "../Styles/Navbar.scss"
-  import React from 'react'
+import React, { useState } from 'react';
+import "../Styles/Navbar.scss";
 
-  const Navbar = () => {
-    return (
-      <div className="navbar">
+const Navbar = () => {
+  const [isHamburgerClicked, setIsHamburgerClicked] = useState(false);
 
-        <div className="navLogo">
-          <a href="#home">
-            <h1>Sulakshan.</h1>
-          </a>
-        </div>
+  const toggleHamburger = () => {
+    setIsHamburgerClicked(!isHamburgerClicked);
+  };
 
-        <div className="navLinks">
-          <ul>
-            <li><a href="#about">About</a></li>
-            {/* <li><a href="#experience">Experience</a></li> */}
-            <li><a href="#projects">Projects</a></li>
-          </ul> 
-        </div>
+  const closeHamburger = () => {
+    setIsHamburgerClicked(false); // Set isHamburgerClicked to false to close the menu
+  };
 
-        <div className="navBtn">
-          <a href="#contact">Contact</a>
-        </div>    
-
+  return (
+    <div className="navbar">
+      <div className="navLogo">
+        <a href="#home">
+          <h1>Sulakshan.</h1>
+        </a>
       </div>
-    )
-  }
 
-  export default Navbar
+      <div className={`${isHamburgerClicked ?  'showOnMobile' : 'navLinks'}`}>
+        <ul>
+          <li><a href="#projects" onClick={closeHamburger}>Projects</a></li>
+          <li><a href="#contact" onClick={closeHamburger}>Contact</a></li>
+        </ul> 
+      </div>
+
+      <div className="navBtn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className={`feather feather-menu ${isHamburgerClicked ? 'open' : ''}`}
+          onClick={toggleHamburger}
+        >
+          {!isHamburgerClicked ? (
+            <>
+              <line x1="3" y1="6" x2="21" y2="6"></line>
+              <line x1="3" y1="12" x2="21" y2="12"></line>
+              <line x1="3" y1="18" x2="21" y2="18"></line>
+            </>
+          ) : (
+            <>
+              <line x1="18" y1="6" x2="6" y2="18"></line>
+              <line x1="6" y1="6" x2="18" y2="18"></line>
+            </>
+          )}
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
+  
