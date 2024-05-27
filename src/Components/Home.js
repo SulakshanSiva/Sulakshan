9502bc89feee4cc8dd649b21bb17b943ab67b7f5
Sulakshan Sivakumaran <sulakshansivak@gmail.com>
2024-05-27 19:51:14 -0400

    updated resume link

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
         <h3 className='jobTitle'>Software Engineer</h3>
         <p>I'm a Full-Stack engineer, currently in the <br></br>Computer Science (Co-op) program at the University of Guelph.</p>
 
-        <a href="https://drive.google.com/file/d/1OIXSgYwDi0j6pq9io2NP1lefIXEW8ev_/view?usp=sharing" target="_blank">
+        <a href="https://drive.google.com/file/d/1Dlr4ra_Pi84QJf5ysHmcnhhipXKWLCbe/view?usp=sharing" target="_blank">
           <button>Resume</button>
         </a>
 
